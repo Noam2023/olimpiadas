c73002a6229af594e496a7a729294070e45cb534
Noam2023 <est.llanez.noam@latecnicalf.com.ar>
2023-09-18 10:43:51 -0300

    Primer commit

---
+++ app/Http/Controllers/PacienteEnfermeroController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente_Enfermero;
+use Illuminate\Http\Request;
+
+class PacienteEnfermeroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Paciente_Enfermero $paciente_Enfermero)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Paciente_Enfermero $paciente_Enfermero)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Paciente_Enfermero $paciente_Enfermero)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Paciente_Enfermero $paciente_Enfermero)
+    {
+        //
+    }
+}
